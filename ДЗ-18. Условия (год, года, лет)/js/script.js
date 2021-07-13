4be4e5a707fdcdeefeb86b137e345c61f9df31d4
YevheniiSkrypkin <81817746+YevheniiSkrypkin@users.noreply.github.com>
2021-07-13 19:33:23 +0300

    Update script.js
    Update

--- ДЗ-18. Условия (год, года, лет)/js/script.js
+++ ДЗ-18. Условия (год, года, лет)/js/script.js
@@ -2,7 +2,7 @@ let number = +prompt('Введите число')
 let last = number.toString().slice(-1)
 let lastException = number.toString().slice(-2)
 
-if (number == isNaN) {
+if (number === isNaN) {
     alert('Error')
 }
 
@@ -10,13 +10,13 @@ if (lastException >= 11 && lastException <= 14) {
     alert(number + ' лет')
 }
 else {
-    if (last == 1) {
+    if (last === 1) {
         alert(number + ' год')
     }
-    else if (last == 2 || last == 3 || last == 4) {
+    else if (last === 2 || last === 3 || last === 4) {
         alert(number + ' годa')
     }
-    else if (last == 5 || last == 6 || last == 7 || last == 8 || last == 9 || last == 0){
+    else if (last === 5 || last === 6 || last === 7 || last === 8 || last === 9 || last === 0){
         alert(number + ' лет')
     }
 }
